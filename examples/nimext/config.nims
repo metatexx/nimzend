493543c8ba9d5ce6cfbb1a34835b8e2cf93c52fb
Hans Raaf <hara@oderwat.de>
2016-05-26 17:51:51 +0200

    More complex example using pegs module.

--- examples/nimext/config.nims
+++ examples/nimext/config.nims
@@ -10,3 +10,4 @@ task tests, "runs a simple test":
   setCommand "nop"
   exec phpExe & """ -r 'dl("""" & extensionName & """.so");  echo nim_fun(2.5, "Hello World!", " - ").PHP_EOL;'"""
   exec phpExe & """ -r 'dl("""" & extensionName & """.so");  echo nim_alpha("1!2N%im3L&4a/ng5").PHP_EOL;'"""
+  exec phpExe & """ -r 'dl("""" & extensionName & """.so");  echo nim_peg("Var1=key1;var2=Key2;   VAR3").PHP_EOL;'"""
--- examples/nimext/nimext.nim
+++ examples/nimext/nimext.nim
@@ -1,11 +1,12 @@
 # nim build --verbosity:0
-# run nim tests
+# !run nim tests
+# run phpr dl("nimext.so"); echo nim_peg("Var1=key1;var2=Key2;   VAR3").PHP_EOL;
 # !run phpr dl("nimext.so"); echo nim_zval("12\0"."3").PHP_EOL;
 # !run phpr dl("nimext.so"); echo nim_alpha("12H!e#0&ll3_o?").PHP_EOL;
 # !run phpr dl("nimext.so"); echo nim_fun(2.5, "Hello World!", " - ").PHP_EOL;
 
 import "../../src/nimzend.nim"
-import math
+import math, strutils, pegs
 
 #
 # my code
@@ -41,5 +42,19 @@ proc nim_zval(zv: ZVal) {.phpfunc.} =
   echo zv.kind
   echo zv.value.str.text
   echo zv.value.str.len
+
+proc nim_peg(str: string): string {.phpfunc.} =
+  proc handleMatches(m: int, n: int, c: openArray[string]): string =
+    result = ""
+
+    if m > 0:
+      result.add ", "
+
+    result.add case n:
+      of 2: c[0].toLower & ": '" & c[1] & "'"
+      of 1: c[0].toLower & ": ''"
+      else: ""
+
+  result = str.replace(peg"{\ident}('='{\ident})* ';'* \s*", handleMatches)
 
 finishExtension("nimext","0.1")
