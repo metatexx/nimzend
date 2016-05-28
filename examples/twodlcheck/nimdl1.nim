ae58cf3316f017fe422ef5c521bd5f45c0ff82e4
Hans Raaf <hara@oderwat.de>
2016-05-28 18:29:40 +0200

    made twodlcheck even harder.

--- examples/twodlcheck/nimdl1.nim
+++ examples/twodlcheck/nimdl1.nim
@@ -1,11 +1,10 @@
 # nim nop
 # run nim build --verbosity:0 nimdl1.nim
 # run nim build --verbosity:0 nimdl2.nim
-# run phpr @dl("nimdl2.so"); @dl("nimdl1.so"); echo dl1(1).dl2().PHP_EOL;
+# run phpr @dl("nimdl1.so"); @dl("nimdl2.so"); echo var_dump(dl1(1)).dl2("test=#id","foo").PHP_EOL;
 
 import "../../src/nimzend.nim"
 
-proc dl1(s2: string): string {.phpfunc.} =
-  result = s2
+proc dl1(s2: string): ZValArray {.phpfunc.} = result.add s2
 
 finishExtension("nimdl1", "0.1")
--- examples/twodlcheck/nimdl2.nim
+++ examples/twodlcheck/nimdl2.nim
@@ -1,14 +1,14 @@
 # nim nop
 # run nim build --verbosity:0 nimdl1.nim
 # run nim build --verbosity:0 nimdl2.nim
-# run phpr @dl("nimdl1.so"); @dl("nimdl2.so"); echo dl1(1).dl2().PHP_EOL;
+# run phpr @dl("nimdl1.so"); @dl("nimdl2.so"); echo var_dump(dl1(1)).dl2("test=#id","foo").PHP_EOL;
 
 import "../../src/nimzend.nim"
 
 import pegs
 
-proc dl2() {.phpfunc.} =
-  let p2 = peg"'test'"
-  returnString p2.repr
+proc dl2(s: string, r: string) {.phpfunc.} =
+  let p2 = peg"'#id'"
+  returnString s.replace(p2,"'" & r & "'")
 
 finishExtension("nimdl2", "0.1")
